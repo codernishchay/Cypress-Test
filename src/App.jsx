20072cbc8e84f986ef003e09422fa8994cc79979
codernishchay <nishchay.vns2000@gmaiil.com>
2022-04-26 06:20:04 -0400

    added layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,40 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const styles = [
+    ["base", "left", "right"],
+    ["base1", "left1", "right1"],
+    ["base2", "left2", "right2"],
+  ];
 
+  const [style, setstyle] = useState(styles[0]);
+  const [couter, setcouter] = useState(0);
+
+  const handletoggle = () => {
+    setcouter((couter + 1) % 3);
+    setstyle(styles[couter]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="app">
+      {style && (
+        <div className={style[0]}>
+          <>
+            <div className={style[1]}>
+              <h2>Hi, left</h2>
+            </div>
+            <div className={style[2]}>
+              <h2> Hi, right </h2>
+            </div>
+          </>
+        </div>
+      )}
+      <button className="button-position" onClick={handletoggle}>
+        Toggle
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
